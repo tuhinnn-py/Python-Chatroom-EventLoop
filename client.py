2c8f92576217366bc6bf90590201e2cd89173ab9
Tuhin Mukherjee <mukherjeetuhin60@gmail.com>
2021-02-18 13:10:09 +0530

    Add files via upload

---
+++ client.py
@@ -0,0 +1,64 @@
+import sys
+import select
+
+import socket
+import msvcrt
+#from server import IP_ADDR, PORT
+
+IP_ADDR = socket.gethostbyname(socket.gethostname())
+PORT = 8000
+HEADER_SIZE = 10
+
+class Connection():
+    def __init__(self, default = (IP_ADDR, PORT)):
+        self.client_conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        #self.client_conn.settimeout(2.0)
+        print(f"Trying to connect to {default[0]} : {default[1]}")
+        self.client_conn.connect(default)
+        
+        print("Connection succesful!")
+        username = input("Enter your username : ")
+        username = f'{len(username) :< {HEADER_SIZE}}' + username
+        self.client_conn.send(username.encode("utf-8"))
+    
+    def fileno(self):
+        return self.client_conn.fileno()
+    
+    def on_read(self):
+        msg = self.client_conn.recv(1024).decode("utf-8")
+        self.decode_message(msg)
+    
+    def decode_message(self, msg):
+        while len(msg):
+            username = msg[HEADER_SIZE : HEADER_SIZE  + int(msg[: HEADER_SIZE])]
+            msg = msg[HEADER_SIZE + int(msg[: HEADER_SIZE]) : ]
+            
+            user_msg = msg[HEADER_SIZE : HEADER_SIZE  + int(msg[: HEADER_SIZE])]
+            msg = msg[HEADER_SIZE + int(msg[: HEADER_SIZE]) : ]
+            
+            print(f"{username} >> {user_msg}")
+
+class Input():
+    def __init__(self, client):
+        self.client = client.client_conn
+    
+    def fileno(self):
+        return sys.stdin.fileno()
+    
+    def on_read(self):
+        #msg = sys.stdin.readline()[:-1]
+        msg = input("[:] >> ")
+        msg_len = len(msg)
+        msg = f'{msg_len :< {HEADER_SIZE}}' + msg
+        
+        self.client.send(msg.encode("utf-8"))
+        
+connection = Connection()
+read_input = Input(connection)
+
+while True:
+    readers, _, _ = select.select([connection], [], [], 1)
+    if(msvcrt.kbhit()):
+        readers.append(read_input)
+    for reader in readers:
+        reader.on_read()
---
+++ server.py
@@ -0,0 +1,93 @@
+import sys
+import select
+
+import msvcrt
+import socket
+
+IP_ADDR = socket.gethostbyname(socket.gethostname())
+PORT = 8000
+HEADER_SIZE = 10
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((IP_ADDR, PORT))
+
+def add_header(username, msg):
+    username = f'{len(username) :< {HEADER_SIZE}}' + username
+    msg_len = len(msg)
+    msg = username + f'{msg_len :< {HEADER_SIZE}}' + msg
+    
+    return msg.encode("utf-8")
+
+sock_list = [server]
+sock_dict = {server : 'Server'}
+
+def broadcast_message(client, broadcast_msg):
+    try: #EAFP
+        client.send(broadcast_msg)
+    except:
+        username = sock_dict[client]
+        del sock_dict[client]
+        sock_list.remove(client)
+        
+        broadcast_msg = add_header(sock_dict[server], f"{username} has left the group!!")
+        for clients in sock_list:
+            if clients is server:
+                print(f"{username} has left the group!!")
+            else:
+                broadcast_message(clients, broadcast_msg)
+
+server.listen()
+while True:
+    readers, _, err_sockets = select.select(sock_list, [], [], 1)
+    if(msvcrt.kbhit()):
+        msg = input("[:] >> ")
+        #msg = sys.stdin.readline()[:-1]
+        msg = add_header(sock_dict[server], msg)
+        
+        for client in sock_list:
+            if client is server:
+                continue
+            else:
+                broadcast_message(client, msg)
+
+    for reader in readers:
+        if reader is server:
+            client_socc, client_addr = server.accept()
+            try:
+                client_username = client_socc.recv(1024).decode("utf-8")
+                if not len(client_username):
+                    continue
+                else:
+                    print(f"Connection accepted from {client_username[HEADER_SIZE : ].title()} : {client_addr[0]} : {client_addr[1]}")
+                    sock_dict[client_socc] = client_username[HEADER_SIZE : ].title()
+                    sock_list.append(client_socc)
+                    
+                    broadcast_msg = add_header(sock_dict[server], f"{sock_dict[client_socc]} has joined the group!!")
+                    for client in sock_list:
+                        if client is server or client is client_socc:
+                            continue
+                        else:
+                            broadcast_message(client, broadcast_msg)
+            except:
+                continue
+        else:
+            try:
+                client_msg = reader.recv(1024).decode("utf-8")
+                if not len(client_msg):
+                    del sock_dict[reader]
+                    sock_list.remove(reader)
+                    
+                else:
+                    while len(client_msg):
+                        broadcast_msg = add_header(sock_dict[reader], client_msg[HEADER_SIZE : HEADER_SIZE + int(client_msg[:HEADER_SIZE])])
+                        print(f"{sock_dict[reader]} >> {client_msg[HEADER_SIZE : HEADER_SIZE + int(client_msg[:HEADER_SIZE])]}")
+                        client_msg = client_msg[HEADER_SIZE + int(client_msg[:HEADER_SIZE]) : ]
+                        
+                        for client in sock_list:
+                            if client is server or client is reader:
+                                continue
+                            else:
+                                broadcast_message(client, broadcast_msg)
+            except:
+                continue
+        
